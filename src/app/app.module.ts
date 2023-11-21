04e0d0121818c362b3420c65f55519b1475def53
Jesus Villalon <villalongallardo92@gmail.com>
2023-11-21 11:45:42 +0100

    web structure created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProyectsComponent } from './components/proyects/proyects.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutMeComponent,
+    DashboardComponent,
+    ProyectsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
