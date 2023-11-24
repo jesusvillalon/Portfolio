966d60c053c56d0252cfd306b0af33cce10ae3e7
Jesus Villalon <villalongallardo92@gmail.com>
2023-11-24 08:26:29 +0100

    updates

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+interface Skill {
+  name: string;
+  img: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent {
+
+
+  public hardSkillsImg: Skill[] = [
+    {name: 'Html', img: "../../../assets/logos/html.svg"},
+    {name: 'Css', img: "../../../assets/logos/css.svg"},
+    {name: 'JavaScript', img: "../../../assets/logos/javascript.svg"},
+    {name: 'Angular', img: "../../../assets/logos/angular.svg"},
+    {name: 'React', img: "../../../assets/logos/react.svg"},
+    {name: 'TypeScript', img: "../../../assets/logos/typescript.svg"},
+    {name: 'Node', img: "../../../assets/logos/node.svg"},
+    {name: 'Express', img: "../../../assets/logos/express.svg"},
+    {name: 'MySQL', img: "../../../assets/logos/mysql.svg"},
+    {name: 'Git', img: "../../../assets/logos/git.svg"},
+    {name: 'Github', img: "../../../assets/logos/github.svg"},
+  ]
 
 }
